1813d4bbeb3d703e1420a7e84a0f1719b9316809
tomas0011 <tomasbenjamin117@gmail.com>
2025-11-19 04:35:52 -0300

    fix test

--- utils/algorithms/knapsackProblem/knapsackTest.cpp
+++ utils/algorithms/knapsackProblem/knapsackTest.cpp
@@ -1,31 +1,29 @@
-#include "knapsack.cpp"   // la versión nueva de la mochila
-#include "../../entities/Package.h"
-#include "../../list/List.h"
+#include "knapsack.h"    // la versión nueva de la mochila
 #include <iostream>
 using namespace std;
 
 int main() {
-    Package p1("Laptop", 3.0, 400.0);
-    Package p2("Celular", 1.0, 250.0);
-    Package p3("Libro", 2.0, 40.0);
-    Package p4("Campera", 1.5, 120.0);
-    Package p5("Zapatillas", 2.5, 150.0);
-    Package p6("Microfono", 1.0, 250.0);
-    Package p7("Auriculares", 0.1, 20.0);
-    Package p8("Diamante", 1, 2000.0);
+    Package p1(1, "Laptop", 400.0, 1, 3.0);
+    Package p2(2, "Celular", 250.0, 1, 1.0);
+    Package p3(3, "Libro", 40.0, 1, 2.0);
+    Package p4(4, "Campera", 120.0, 1, 1.5);
+    Package p5(5, "Zapatillas", 150.0, 1, 2.5);
+    Package p6(6, "Microfono", 250.0, 1, 1.0);
+    Package p7(7, "Auriculares", 20.0, 1, 0.1);
+    Package p8(8, "Diamante", 2000.0, 10, 1.0);
 
     // -----------------------------
     // CAMBIO: usamos List en lugar de vector
     // -----------------------------
     List paquetes;
-    paquetes.push(p1);
-    paquetes.push(p2);
-    paquetes.push(p3);
-    paquetes.push(p4);
-    paquetes.push(p5);
-    paquetes.push(p6);
-    paquetes.push(p7);
-    paquetes.push(p8);
+    paquetes.push(any(p1));
+    paquetes.push(any(p2));
+    paquetes.push(any(p3));
+    paquetes.push(any(p4));
+    paquetes.push(any(p5));
+    paquetes.push(any(p6));
+    paquetes.push(any(p7));
+    paquetes.push(any(p8));
 
     double pesoMaximo = 5.0;
 
@@ -35,12 +33,19 @@ int main() {
     cout << "PRECIO MAXIMO: " << r.precioMaximo << endl;
     cout << "PAQUETES SELECCIONADOS:" << endl;
 
-    for (auto& p : r.paquetesSeleccionados) {
-        cout << " - " << p.getId()
-            << " | peso: " << p.getWeight()
-            << " | valor: " << p.getValue()
-            << endl;
-    }
+    Node* nodo = r.paquetesSeleccionados.getHead();
+        while (nodo != nullptr) {
+            Package p = any_cast<Package>(nodo->getData());
+            cout << " - ID " << p.getId()
+                << " | Nombre: " << p.getName()
+                << " | Peso: " << p.getWeight()
+                << " | Valor: " << p.getPrice()
+                << endl;
 
-    return 0;
+            nodo = nodo->getNext();
+        }
+
+        cout << "==============================" << endl;
+
+        return 0;
 }
